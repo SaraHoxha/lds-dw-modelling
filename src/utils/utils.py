4b1db76f73314d8985e47f05298b0e89fecf2508
Sara Hoxha <sarahoxha99@gmail.com>
2024-11-07 23:25:19 +0100

    changed location of 'data_cleaning_people' for rn, added method to fill missing values with defaults

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -8,3 +8,11 @@ def check_if_file_exists_and_create (filePath):
     if not os.path.exists(filePath):
         with open(filePath, 'w') as f:
             pass
+        
+
+def fill_missing_values (dataset, column_defaults):
+    for row_key, row in dataset.items():
+        for column, default_value in column_defaults.items():
+            if  row[column] is None:
+                row[column] = default_value
+    return dataset
