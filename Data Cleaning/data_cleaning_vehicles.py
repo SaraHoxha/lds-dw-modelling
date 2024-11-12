d2a07f800f7ee9b1e33659c92cd4b6214fd4ad55
RafaelUrbina <riurbinah@gmail.com>
2024-11-12 19:19:00 +0100

    Commiting changes from yesterday's meet

---
+++ Data Cleaning/data_cleaning_vehicles.py
@@ -0,0 +1,49 @@
+#Imports
+from utils.read_write import read_csv, to_csv
+from utils.utils import fill_missing_values, split_date
+from utils.vehicles_utils import license_xx_to_u, typos_fixing, set_vehicle_year
+
+# Read "Vehicles csv"
+vehicles_df = read_csv ('../data/Vehicles.csv', 'CRASH_UNIT_ID')
+
+# Fill missing values for columns that provide a default 'Unknown' value
+column2defaultUknown = {
+'UNIT_TYPE': 'UNKNOWN',
+'LIC_PLATE_STATE': 'UNKNOWN',
+'MAKE': 'UNKNOWN',
+'MODEL': 'UNKNOWN',
+'VEHICLE_DEFECT': 'UNKNOWN',
+'VEHICLE_TYPE': 'UNKNOWN/NA',
+'VEHICLE_USE': 'UNKNOWN/NA',
+'TRAVEL_DIRECTION': 'UNKNOWN', 
+'MANEUVER': 'UNKNOWN',
+'FIRST_CONTACT_POINT': 'UNKNOWN'
+}
+
+vehicles_df_processed = fill_missing_values(vehicles_df, column2defaultUknown)
+
+#Changing XX state to unknown
+vehicles_df_processed = license_xx_to_u(vehicles_df_processed)
+
+#Typos for brand and fixing them
+typos_make = {'NEW HOLLAND, DIV. OF SPERRY NEW HOLLAND': 'NEW HOLLAND, (DIV. OF SPERRY NEW HOLLAND)',
+    'AMC (LAWN & GARDEN TRACTORS BY AMERICAN MOTORS)': 'AMC (LAWN & GARDEN TRACTORS BY AMMERICAN MOTORS)',
+    'ROLLS ROYCE': 'ROLLS-ROYCE'}
+
+vehicles_df_processed = typos_fixing(vehicles_df_processed, typos_make,"MAKE")
+
+#Typos for model and fixing them
+typos_model = {'UNKNOWN':'UNKOWN'}
+
+vehicles_df_processed = typos_fixing(vehicles_df_processed,typos_model,"MODEL")
+
+#Fixing years of vehicles
+
+vehicles_df_processed = set_vehicle_year(vehicles_df_processed)
+
+#Splitting time into 
+date_column = 'CRASH_DATE'
+people_df_processed =  split_date(vehicles_df_processed, date_column)
+
+#writing to csv
+to_csv(vehicles_df_processed,'../data/Vehicles_Processed.csv')
--- Data Cleaning/utils/vehicles_utils.py
+++ Data Cleaning/utils/vehicles_utils.py
@@ -5,8 +5,11 @@ VEHICLE_YEAR = "VEHICLE_YEAR"
 # Make following mapping for 'LIC_PLATE_STATE' values: XX values -> UNKNOWN
 def license_xx_to_u(dataset):
     for row in dataset.values():
-        if LICENCE_PLATE in row and row[LICENCE_PLATE] == 'XX':
-            row[LICENCE_PLATE] = "UNKNOWN"
+        try:
+            if LICENCE_PLATE in row and row[LICENCE_PLATE] == 'XX':
+                row[LICENCE_PLATE] = "UNKNOWN"
+        except Exception as e:
+             print(f"Error processing {LICENCE_PLATE} | Error: {e}")
     return dataset
 
 #Typo fixing for the MAKE of cars:
@@ -18,18 +21,24 @@ def license_xx_to_u(dataset):
 #('UNKNOWN', 'UNKOWN')
 def typos_fixing(dataset,typos,attribute):
     for row in dataset.values():
-        for true_val, typo_val in typos.items():
-                if row[attribute] == typo_val:
-                    row[attribute] = true_val       
+        try:
+            for true_val, typo_val in typos.items():
+                    if row[attribute] == typo_val:
+                        row[attribute] = true_val  
+        except Exception as e:
+             print(f"No typos values in {attribute} | Error: {e}")     
     return dataset
 
 # Set a nan value for 'VEHICLE_YEAR' for observations when "VEHICLE_YEAR" > 2024
 def set_vehicle_year(dataset):
     for row in dataset.values():
-                if  row.get(VEHICLE_YEAR):
-                    if float(row[VEHICLE_YEAR]) == 9999:
-                         row[VEHICLE_YEAR] = float(1999)
-                    elif float(row[VEHICLE_YEAR]) > 2024:
-                        row[VEHICLE_YEAR] = float('nan')
+                try:
+                    if  row.get(VEHICLE_YEAR):
+                        if float(row[VEHICLE_YEAR]) == 9999:
+                             row[VEHICLE_YEAR] = float(1999)
+                        elif float(row[VEHICLE_YEAR]) > 2024:
+                            row[VEHICLE_YEAR] = float('nan')
+                except Exception as e:
+                    print(f"Error processing {VEHICLE_YEAR} | Error: {e}")
     return dataset
 
